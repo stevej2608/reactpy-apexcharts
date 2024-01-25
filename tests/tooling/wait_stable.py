bcf337ce2216c46c3d9497bd806404bb7c0b20fc
Steve Jones <jonesst2608@gmail.com>
2024-01-25 13:59:16 +0000

    Reduce wait_stable time, now 2000ms

--- tests/tooling/wait_stable.py
+++ tests/tooling/wait_stable.py
@@ -2,4 +2,4 @@
 async def wait_page_stable(page):
     await page.wait_for_load_state("networkidle")
     await page.wait_for_load_state("domcontentloaded")
-    await page.wait_for_timeout(5000)
+    await page.wait_for_timeout(2000)
